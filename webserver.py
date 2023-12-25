44bc4305a17b4611439e5afb709e452fa4f730db
Veselin Delchev <delchev.v5454@gmail.com>
2023-12-25 14:20:10 +0200

    Camera is working now
    Found web cam, manage to display cords on the mouse movement

--- webserver.py
+++ webserver.py
@@ -6,19 +6,23 @@ from webcam_capture import capture_webcam_image
 from database import save_to_database
 
 async def handle(request):
-    return web.Response(text=open('index.html').read(), content_type='text/html')
+    content = open('index.html', 'r').read()
+    return web.Response(text=content, content_type='text/html')
 
 async def websocket_handler(request):
     ws = web.WebSocketResponse()
     await ws.prepare(request)
 
-    while True:
-        mouse_coords = get_mouse_coordinates()
-        image_data = capture_webcam_image()
+    try:
+        while True:
+            mouse_coords = get_mouse_coordinates()
+            image_data = capture_webcam_image()
 
-        await ws.send_str(f"Mouse Coordinates: {mouse_coords}")
+            await ws.send_str(f"Mouse Coordinates: {mouse_coords}")
 
-        await asyncio.sleep(1)
+            await asyncio.sleep(1)
+    finally:
+        await ws.close()
 
 app = web.Application()
 app.add_routes([web.get('/', handle), web.get('/ws', websocket_handler)])
